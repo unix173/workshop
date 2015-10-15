3000bc53e08714cd4d92c80329c16cea020df5c2
Ivan Simic <simicivan173@gmail.com>
2015-10-15 10:43:32 +0200

    Initial commit

---
+++ vid.py
@@ -0,0 +1,13 @@
+# Stream video from webcam to local window
+# Usage: video
+
+import cv2
+import numpy as np
+c = cv2.VideoCapture(0)
+
+while(1):
+    _,f = c.read()
+    cv2.imshow('e2',f)
+    if cv2.waitKey(5)==27:
+        break
+cv2.destroyAllWindows()
